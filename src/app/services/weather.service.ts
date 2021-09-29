115eaab54ddb92a4bcc4fc8eefa180ff3efbc51d
Atiqul Islam <atiqul.islam@gmail.com>
2021-09-29 13:41:11 -0400

    Adding weather service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import 'rxjs/Rx';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  baseUrl: string= "";
+  units:string = "metrics";
+  url:string = "";
+  forecast: any;
   public apiKey:string = "cf9d4e833a4f993b0214cf67d1350a08";
-  constructor() { }
+  constructor(private http:HttpClient) {
+    this.baseUrl = "http://api.openweathermap.org/data.2.5/";
+    this.appKey
+  }
 }
